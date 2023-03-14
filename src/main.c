001ecee76124107e6a83fd35abe1a1ae72328e44
Gervais Pierre <94633856+GervaisPierre@users.noreply.github.com>
2023-03-14 11:18:25 +0100

    buttonLed & dual volt reading to serial port

--- src/main.c
+++ src/main.c
@@ -8,13 +8,24 @@ int main()
 {
   analogInit();
   init_block_USART0();
-  
+
+  init_input_GPIO(D3, PULLUP);
+  init_output_GPIO(D7);
+
+  int count = 0;
+
   while(1) {
 
-    printf("------------------------\n");
-    checkPotentiometerForSerial(A0);
-    checkPotentiometerForSerial(A1);
+    if (count >= 100) {
+      printf("------------------------\n");
+      checkPotentiometerForSerial(A0);
+      checkPotentiometerForSerial(A1);
+      count = 0;
+    }
+    
+    checkButtonForLed(D3, D7);
 
-    _delay_ms(1000);
+    _delay_ms(10);
+    count++;
   }
 }
